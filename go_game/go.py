bdc8a1f117b7f811a3a3be418597180ba93b40a0
Klahadore <stringycheese@protonmail.com>
2025-01-01 17:05:08 -0500

    created game

---
+++ go_game/go.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def initialize_state():
+    return
+
+def get_legal_moves(player, state):
+    pass
+
+def apply_move(player, state, move):
+    pass
+
+def is_terminal(player, state, move):
+    pass
+
+def scoring(state):
+    pass
